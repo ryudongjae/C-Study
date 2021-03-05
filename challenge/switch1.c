6c2688e4e8fac2e26c3a020c269dd764bfd68fd4
ryudongjae <rdj10149@gmail.com>
2021-03-05 22:06:32 +0900

    Commit

--- challenge/switch1.c
+++ challenge/switch1.c
@@ -8,10 +8,13 @@ int main(void){
     {
     case 1: case 2: case 3:
         printf("봄\n"); break;
+    
     case 4: case 5: case 6:
         printf("여름\n"); break;
+    
     case 7: case 8: case 9:
         printf("가을\n"); break;
+    
     case 10: case 11: case 12:
         printf("겨울\n"); break;
     
